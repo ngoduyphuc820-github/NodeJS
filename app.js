0cc192533e4efcfa9d76e92baf3e3523d0695ef6
NDP <ngoduyphuc820@gmail.com>
2021-12-16 16:21:43 +0700

    add app.js and users.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const userRouters = require('./routes/users');
+
+app.use(express.json());
+app.use('/users', userRouters);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log('Your app running on port ' + port);
+});
